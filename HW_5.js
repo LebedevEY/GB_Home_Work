283d9c910f822fdb961ec0aed5b8a22aeca70824
Evgeniy Lebedev <evilhamster@yandex.ru>
2021-06-05 15:43:59 +0300

    HW_5

--- HW_5.js
+++ HW_5.js
@@ -1,16 +1,8 @@
 'use strict';
 
-const settigns = {
-    headCounts: 8,
-    rowCounts: 8,
-    colCounts: 8,
-}
-
 const game = {
-    settigns,
     containerElement: null,
     cellElements: [],
-    thName: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
 
     run() {
         this.init();
@@ -22,27 +14,38 @@ const game = {
     },
 
     initCells() {
-        this.containerElement.innerHTML = '';
-        this.cellElements = [];
+        const cols = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 0];
+        const rows = [0, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
-        for (let heading = 0; heading < this.settigns.headCounts; heading++) {
-            const th = document.createElement('th');
-            this.containerElement.appendChild(th);
-            th.innerHTML = this.thName[heading];
-        }
-
-        for (let row = 0; row < this.settigns.rowCounts; row++) {
+        for (let row = 0; row < rows.length; row++) {
             const tr = document.createElement('tr');
             this.containerElement.appendChild(tr);
 
-            for (let col = 0; col < this.settigns.rowCounts; col++) {
+            for (let col = 0; col < cols.length; col++) {
                 const td = document.createElement('td');
                 tr.appendChild(td);
 
-                this.cellElements.push(td);
+                if (rows[row] === 0 && cols[col] !== 0) {
+                    td.innerHTML = cols[col];
+                } else if (cols[col] === 0 && rows[row] !== 0) {
+                    td.innerHTML = rows[row].toString();
+                }
+
+                if (this.isCellIsBlack(row, col)) {
+                    td.style.backgroundColor = 'black';
+                }
             }
         }
     },
-}
+
+    isCellIsBlack(rowNum, colNum) {
+
+        if (rowNum === 0 || colNum === 0 || rowNum === 9 || colNum === 9) {
+            return false;
+        }
+
+        return (rowNum + colNum) % 2 === 1;
+    },
+};
 
 game.run();
---
+++ cart.js
@@ -0,0 +1,82 @@
+const cartItem = {
+    render(good) {
+        return `<div class="good">
+                    <div><b>Наименование</b>: ${good.product_name}</div>
+                    <div><img class = "product_img" src="img/${good.product_photo}" alt="product image"></div>
+                    <div><b>Цена за шт.</b>: ${good.price}</div>
+                    <div><b>Количество</b>: ${good.quantity}</div>
+                    <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                </div>`;
+    }
+}
+
+const cart = {
+    cartListBlock: null,
+    cartButton: null,
+    cartItem,
+    goods: [
+        {
+            id: 1,
+            product_name: 'Ноутбук',
+            product_photo: 'laptop.png',
+            price: 80000,
+            quantity: 1
+        },
+
+        {
+            id: 2,
+            product_name: 'МФУ',
+            product_photo: 'mfd.png',
+            price: 50000,
+            quantity: 2
+        },
+
+        {
+            id: 3,
+            product_name: 'Клавиатура',
+            product_photo: 'keyboard.png',
+            price: 2000,
+            quantity: 7
+        },
+
+        {
+            id: 4,
+            product_name: 'Мышь',
+            product_photo: 'mouse.png',
+            price: 800,
+            quantity: 7
+        }
+    ],
+
+    init() {
+        this.cartListBlock = document.querySelector('.cart-list');
+        this.cartButton = document.querySelector('.cart-btn');
+        this.cartButton.addEventListener('click', this.clearCart.bind(this));
+
+        this.render();
+    },
+
+    render() {
+        if (this.goods.length) {
+            this.goods.forEach(good => {
+                this.cartListBlock.insertAdjacentHTML('beforeend', this.cartItem.render(good));
+            });
+            this.cartListBlock.insertAdjacentHTML('beforeend', `В корзине ${this.goods.length} позиций(я) стоимостью: ${this.getCartPrice()}`);
+        } else {
+            this.cartListBlock.textContent = 'Корзина пуста';
+        }
+    },
+
+    getCartPrice() {
+        return this.goods.reduce(function (price, good) {
+            return price + good.price * good.quantity;
+        }, 0);
+    },
+
+    clearCart() {
+        this.goods = [];
+        this.render();
+    },
+};
+
+cart.init();
